ed2e177228fbbb30478ca9a9e03bed0703d0fd81
Sébastien Henau <sebastien@spatie.be>
2026-02-25 11:59:36 +0100

    add flare to the project

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,19 +1,31 @@
+import flareSourcemapUploader from '@flareapp/vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-    plugins: [tailwindcss(), react(), vue()],
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                js: resolve(__dirname, 'js/index.html'),
-                react: resolve(__dirname, 'react/index.html'),
-                vue: resolve(__dirname, 'vue/index.html'),
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+
+    return {
+        plugins: [
+            tailwindcss(),
+            react(),
+            vue(),
+            flareSourcemapUploader({
+                key: env.VITE_FLARE_API_KEY,
+            }),
+        ],
+        build: {
+            rollupOptions: {
+                input: {
+                    main: resolve(__dirname, 'index.html'),
+                    js: resolve(__dirname, 'js/index.html'),
+                    react: resolve(__dirname, 'react/index.html'),
+                    vue: resolve(__dirname, 'vue/index.html'),
+                },
             },
         },
-    },
+    };
 });
