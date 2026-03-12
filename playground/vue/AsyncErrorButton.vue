6037d7b78d04450c8f6ab2395b727e0f5f36251f
Sébastien Henau <sebastien@spatie.be>
2026-03-12 10:59:23 +0100

    feat: update vue playground scenario’s

---
+++ playground/vue/AsyncErrorButton.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { onMounted, ref, watch } from 'vue';
+
+import Button from './Button.vue';
+
+const trigger = ref(false);
+
+watch(trigger, (value) => {
+    if (value) {
+        trigger.value = false;
+        console.log('Triggering async error in watch');
+        Promise.reject(new Error('Async error in Vue watch'));
+    }
+});
+</script>
+
+<template>
+    <Button @click="trigger = true">Async error in watch</Button>
+</template>
