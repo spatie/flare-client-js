8d6fc98c973696d3947b4e85ae8eccaf543881bb
Dries Heyninck <dries.heyninck@gmail.com>
2026-05-18 14:02:17 +0200

    chore: clean up debug logging in playground hooks

--- playground-sveltekit/src/hooks.client.ts
+++ playground-sveltekit/src/hooks.client.ts
@@ -1,8 +1,3 @@
 import { handleErrorWithFlare } from '@flareapp/sveltekit/client';
 
-export const handleError = handleErrorWithFlare({
-    beforeSubmit: ({ error, context }) => {
-        console.log('[hooks.client.ts] handleError caught:', error.message);
-        return context;
-    },
-});
+export const handleError = handleErrorWithFlare();
