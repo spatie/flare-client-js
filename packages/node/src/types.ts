0cc0f2499a06a528c8e57c0fff555fe22b0beb07
Dries Heyninck <dries.heyninck@gmail.com>
2026-05-28 16:35:06 +0200

    feat(node): add RequestContext, User, NodeOptions types

---
+++ packages/node/src/types.ts
@@ -0,0 +1,37 @@
+export type RequestContext = {
+    method?: string;
+    path?: string;
+    url?: string;
+    headers?: Record<string, string | string[] | undefined>;
+    body?: unknown;
+};
+
+export type User = {
+    id?: string | number;
+    email?: string;
+    username?: string;
+    ipAddress?: string;
+};
+
+export type FatalMode = 'off' | 'report' | 'report-and-exit';
+
+export type NodeOptions = {
+    uncaughtExceptionMode?: FatalMode;
+    unhandledRejectionMode?: FatalMode;
+    shutdownTimeoutMs?: number;
+    headerDenylist?: RegExp;
+    headerAllowlist?: RegExp;
+    replaceDefaultHeaderDenylist?: boolean;
+    captureRequestBody?: boolean;
+    bodyMaxBytes?: number;
+    bodyAllowedContentTypes?: RegExp;
+    bodyKeyDenylist?: RegExp;
+};
+
+export type ResolvedNodeOptions = Required<
+    Omit<NodeOptions, 'headerDenylist' | 'headerAllowlist' | 'bodyKeyDenylist'>
+> & {
+    headerDenylist: RegExp;
+    headerAllowlist: RegExp | null;
+    bodyKeyDenylist: RegExp;
+};
